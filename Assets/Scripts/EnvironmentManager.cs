1dfba37d8fd9626e1837778287b0f1396a4f4e1a
maxwell1023 <123422476+maxwell1023@users.noreply.github.com>
2023-02-06 19:22:17 -0500

    Started "Treadmill" Environment
    I created a script that moves the tiles down by 1 space if W is pressed and restarts them at the top if M is pressed. This is utilized in the Environment Test Scene.

--- Assets/Scripts/EnvironmentManager.cs
+++ Assets/Scripts/EnvironmentManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class EnvironmentManager : MonoBehaviour
 {
+    [SerializeField] private GameObject groundPlaneW, groundPlaneG, environmentTop; //temporary Test
+    [SerializeField] private GameObject[] environmentPlanes; //temporary Test
     // Start is called before the first frame update
     void Start()
     {
-        
+        environmentPlanes = GameObject.FindGameObjectsWithTag("Plane");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            GameObject bottomObject = environmentPlanes[0];
+            for (int i = 0; i < environmentPlanes.Length; i++)
+            {
+                if (environmentPlanes[i].gameObject.transform.position.z <= bottomObject.transform.position.z)
+                {
+                    bottomObject = environmentPlanes[i];
+                }
+            }
+            SpawnAtTop(bottomObject);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            for (int i = 0; i < environmentPlanes.Length; i++)
+            {
+                if (environmentPlanes[i].gameObject.activeInHierarchy)
+                {
+                    environmentPlanes[i].gameObject.transform.position = new Vector3(environmentPlanes[i].gameObject.transform.position.x, environmentPlanes[i].gameObject.transform.position.y, environmentPlanes[i].gameObject.transform.position.z - 1);
+                }
+            }
+           
+        }
+    }
+
+
+    private void SpawnAtTop(GameObject nextInSpawn)
+    {
+        nextInSpawn.SetActive(false);
+        nextInSpawn.gameObject.transform.position = environmentTop.transform.position;
+
+        nextInSpawn.SetActive(true); //THIS NEEDS TO CHANGE FOR RANDOMIZATION!!!
     }
 }

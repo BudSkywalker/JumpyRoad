34b3e4fa94c2cabaa4145ef73e15c9c7611c8879
ChaosQueenGod <daguytoday@gmail.com>
2023-02-06 19:37:20 -0500

    Game Manager added
    Blocked out sections of the game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver;
+    public bool paused;
+    public bool onMenu;
+
+    public int highscore;
+    private int score;
+
+    //Singleton
+
+
+    //Game states
+    public void YouWin()
+    {
+
+    }
+
+    public void YouLose()
+    {
+
+    }
+
+    public void ResetGame()
+    {
+
+    }
+
+    public void PauseGame()
+    {
+
+    }
+
+    public void UnpauseGame()
+    {
+
+    }
+
+    //Score system
+    private void CheckHighscore()
+    {
+
+    }
+
+    public void SetScore()
+    {
+
+    }
+
+    //Settings
+    public void SetVolume(float volumeNum)
+    {
+
+    }
+
+    public void Fullscreen(bool isFullscreen)
+    {
+
+    }
+}
